78eaf20335765db89f2b10ab7f94a66bacaade49
akiwarheit <kevinjd@umich.edu>
2024-08-04 21:46:53 +1200

    Adding training script, updating inference

--- api/inference.py
+++ api/inference.py
@@ -10,7 +10,7 @@ def query_endpoint(payload, aws_access_key_id, aws_secret_access_key):
         aws_secret_access_key=aws_secret_access_key
     )
     response = client.invoke_endpoint(
-        EndpointName=endpoint_name, InferenceComponentName='huggingface-text2text-flan-t5-xl-20240802-000919', ContentType="application/json", Body=json.dumps(payload).encode("utf-8")
+        EndpointName=endpoint_name, ContentType="application/json", Body=json.dumps(payload).encode("utf-8")
     )
     model_predictions = json.loads(response["Body"].read())
     generated_text = model_predictions[0]["generated_text"]
